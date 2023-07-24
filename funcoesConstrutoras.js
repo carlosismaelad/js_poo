53c0123dc4d5659c9d7131fcb36e74213237027c
carlosismaelad <carlosismaelad@outlook.com>
2023-07-23 22:53:49 -0300

    Correção na aula funcoesConstrutoras.js

--- funcoesConstrutoras.js
+++ funcoesConstrutoras.js
@@ -1,12 +1,12 @@
 //funcoes construtoras são nomeadas usando Pascal-Case CreatesBook
 // O que passaríamos como parâmetros podemos passar diretamente na função
 // usando a palavra reservada this
-function CreatesBook(title, pages, tags, author){ 
-    this.title,
-    this.pages,
-    this.publication,
-    this.author,
-    this.tags
+function CreatesBook(title, pages, publication, tags, author, inStock){ 
+    this.title = title,
+    this.pages = pages,
+    this.publication = publication,
+    this.author = author,
+    this.tags = tags
     this.inStock = 0,
     this.addOnStock = function addOnStock(quantity){
         this.inStock += quantity
@@ -19,7 +19,10 @@ function CreatesBook(title, pages, tags, author){
 const author = {name: 'Andew hunt e David Thomas'}
 const tags = ["Ciência", " Educação", " Programação", " Desenvolvimento"]
 
-const cleanCode = new CreatesBook('Clean code', 425, tags, author)
+
+
+const cleanCode = new CreatesBook('Clean code', 425, 2009, tags, author)
+
 
 console.log(cleanCode)
 
